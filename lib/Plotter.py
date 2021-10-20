baf7e09503aabfc54e83946772ef004703e1e61a
ediah <60201514+ediah@users.noreply.github.com>
2021-10-20 14:12:32 +0300

    На графиках теперь не доля, а количество (людей с таким баллом)

--- lib/Plotter.py
+++ lib/Plotter.py
@@ -15,16 +15,19 @@ class Plotter:
         varUnique = list(set(self.var))
         varCounts = [self.var.count(x) for x in varUnique]
         varInt = list(map(round, map(float, self.var)))
-        
+        varKoef = sum(varCounts)
+        varMax  = max(varCounts)
+                
         fig, ax = plt.subplots()
-        ax.set_ylim([0, 0.12])
-        n, bins, patches = ax.hist(varInt, round(len(varUnique) / 1.2), density=True)
-        y = ((1 / (np.sqrt(2 * np.pi) * sd)) * np.exp(-0.5 * (1 / sd * (bins - mean))**2))
-        ax.plot(bins, y, '--')
-        ax.set_title('$\mu=' + str(mean) + '$ $\sigma=' + str(sd) + '$')
+        ax.set_ylim([0, max(65, varMax)])
+        n, bins, patches = ax.hist(varInt, round(len(varUnique) / 1.2))
+        y = (1 / (np.sqrt(2 * np.pi) * sd)) * np.exp(-0.5 * ((bins - mean) / sd)**2)
+        ax.plot(bins, varKoef * y, '--')
+        
         now = datetime.datetime.now()
         name = str(now).split()[0]
-        pathname = self.img_dir + name + '.png'
+        ax.set_title(name + ' ($\mu=' + str(mean) + '$, $\sigma=' + str(sd) + '$)')
         
+        pathname = self.img_dir + name + '.png'
         if (not os.path.isfile(pathname)) or (now.hour <= 12):
             plt.savefig(pathname, format='png') 
